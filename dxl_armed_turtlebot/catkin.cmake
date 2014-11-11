3913d9f688fc89a4f14534b9d3250ebfb10709ee
Kei Okada <kei.okada@gmail.com>
2014-11-11 16:32:27 +0900

    remove rosbuild code

--- dxl_armed_turtlebot/catkin.cmake
+++
@@ -1,6 +0,0 @@
-cmake_minimum_required(VERSION 2.8.3)
-project(dxl_armed_turtlebot)
-
-find_package(catkin REQUIRED COMPONENTS kobuki_msgs linux_hardware)
-
-catkin_package()
--- dynamixel_7dof_arm/catkin.cmake
+++
@@ -1,6 +0,0 @@
-cmake_minimum_required(VERSION 2.8.3)
-project(dynamixel_7dof_arm)
-
-find_package(catkin REQUIRED COMPONENTS dynamixel_controllers)
-
-catkin_package()
