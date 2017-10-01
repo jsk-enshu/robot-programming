28a3e39345e1a37f7c819b891ba2a158bdcca94a
Yasu31 <yasu313nori@gmail.com>
2017-10-01 14:49:21 +0900

    commented python script

--- dxl_armed_turtlebot/scripts/script.py
+++ dxl_armed_turtlebot/scripts/script.py
@@ -1,17 +1,18 @@
 #! /usr/bin/env python
-import rospy
+import rospy    #python client library for ROS
 from opencv_apps.msg import RotatedRectStamped
-from image_view2.msg import ImageMarker2
+from image_view2.msg import ImageMarker2    #image_view2 allows for drawing markers on image
+# ImageMarker2 is a message to draw on the image_view2 canvas
 from geometry_msgs.msg import Point
 
 def cb(msg):
-    print msg.rect
-    marker=ImageMarker2()
+    print msg.rect  #print data received
+    marker=ImageMarker2()   #create instance of ImageMarker2
     marker.type=0
-    marker.position=Point(msg.rect.center.x, msg.rect.center.y, 0)
+    marker.position=Point(msg.rect.center.x, msg.rect.center.y, 0)  #define position
     pub.publish(marker)
 
 rospy.init_node("client")
 rospy.Subscriber("/camshift/track_box", RotatedRectStamped, cb)
-pub=rospy.Publisher("/image_marker", ImageMarker2)
-rospy.spin()
+pub=rospy.Publisher("/image_marker", ImageMarker2)  #handle to publish messages to a topic
+rospy.spin()    #keeps the node existing, until node is shutdown
