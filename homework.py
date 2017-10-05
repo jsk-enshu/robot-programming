edfc18fed7e4ebb7855ab237ebd3ed23c531c71d
juliuscueto <juliuscueto@gmail.com>
2017-10-05 16:44:32 +0900

    test

---
+++ homework.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+#! This is a test pull request
+import rospy
+from opencv_apps.msg import RotatedRectStamped
+from image_view2.msg import ImageMarker2
+from geometry_msgs.msg import Point
+
+def cb(msg):
+    print msg.rect
+    marker = ImageMarker2()
+    marker.type = 0
+    marker.position = Point(msg.rect.center.x, msg.rect.center.y,0)
+    pub.publish(marker)
+
+rospy.init_node('client')
+rospy.Subscriber('/camshift/track_box',RotatedRectStamped, cb)
+pub = rospy.Publisher('/image_marker',ImageMarker2)
+rospy.spin()
