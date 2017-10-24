0681f344fa7e749621b94595c95cdd175bfeb087
koga-yu <kukasyuto@gmail.com>
2017-10-24 18:33:54 +0900

    日本語コメント動かない euslispのtalker/lister追加

--- scripts/image_marker.py
+++ scripts/image_marker.py
@@ -7,19 +7,12 @@ from geometry_msgs.msg import Point
 
 def cb(msg):
     print msg.rect
-    #markerオブジェクトを初期化する
     marker = ImageMarker2()
     marker.type = 0
-    #長方形の座標をpositionに設定する
     marker.position = Point(msg.rect.center.x, msg.rect.center.y, 0)
-    #メッセージを送る
     pub.publish(marker)
 
-#"client"というノードを作成
 rospy.init_node('client')
-#Subscriberを作成
 rospy.Subscriber('/camshift/track_box', RotatedRectStamped, cb)
-#Publisherを作成
 pub = rospy.Publisher('/image_marker', ImageMarker2)
-#プロセスが終了するまで回す
 rospy.spin()
